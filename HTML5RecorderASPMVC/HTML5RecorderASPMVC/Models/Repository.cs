384b389c16a9b53c729a9ca416c99e3e9c9a1377
JackyLee <Achilles0915@yandex.com>
2019-04-02 01:15:23 -0700

    html5 recorder integrate asp.net mvc5

---
+++ HTML5RecorderASPMVC/HTML5RecorderASPMVC/Models/Repository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace HTML5RecorderASPMVC.Models
+{
+    public class Repository
+    {
+        private string _connectionString;
+
+        public Repository()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+        }
+
+        internal bool SaveFileToDb(string str)
+        {
+            var queryString = "insert into dbo.RecorderFiles (FileName) values (@FileName)";
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var command = new SqlCommand(queryString, connection);
+                connection.Open();
+                try
+                {
+                    command.Parameters.AddWithValue("@FileName", str);
+                    int id = command.ExecuteNonQuery();
+                    if (id > 0)
+                        return true;
+                    return false;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ HTML5RecorderASPMVC/HTML5RecorderASPMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HTML5RecorderASPMVC.Startup))]
+namespace HTML5RecorderASPMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
